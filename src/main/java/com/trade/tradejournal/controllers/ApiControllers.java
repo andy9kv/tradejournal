d0bbdbbcf64704bf99471af9add449e3b4eb554f
davey108 <sligar.david@gmail.com>
2021-03-06 13:19:05 -0500

    Added controllers

---
+++ src/main/java/com/trade/tradejournal/controllers/ApiControllers.java
@@ -0,0 +1,10 @@
+package com.trade.tradejournal.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/trade")
+public class ApiControllers {
+
+}
